9e2c8f9d78254c8c2b7029a4791adb6b9b571f76
Xinyue <poyloust@gmail.com>
2019-04-21 17:31:41 -0400

    added local db

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 var express = require('express');
-var mustache = require('mustache-express');
+var mustacheExpress = require('mustache-express');
 var request = require('request');
-var { Client } = require('pg');
 
-var app = express;
+var { Client } = require('pg');
+var connectionString = process.env.DATABASE_URL;
+
+var app = express();
 var port = 3000 || process.env.PORT;
-var client = new Client({database:'posts-test'});
+var client = new Client({
+    database:'posts-test',
+    connectionString:connectionString,
+    // ssl: true,
+});
 
 client.connect();
 
 client.query('SELECT * FROM posts', function(err,res){
-    if(err) throw err;
-    console.log(res);
+    if (err){
+        console.log(err.stack);
+    }
+    console.log(res.rows);
+
+    client.end();
 });
+//app.use(express.static('static'));
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname);
 
-app.get('/',function(req,res){
-    //
-    mustache.render(something);
-
+app.get("/", function(req,res){
+    console.log('Hello World'); 
+    res.render('forum',{
+        postContent:"bla",
+    });
 });
-
 
 app.get('/post', function(req,res){
-
+    console.log('new post')
     //
 });
 
@@ -30,4 +44,3 @@ app.listen(port, function(){
     console.log('listening port' + port); 
 })
 
-client.end();
