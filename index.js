2b30754d8280d0cfdc495dde6325515fc0594753
Xinyue <poyloust@gmail.com>
2019-04-21 22:35:24 -0400

    update data with inputs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mustacheExpress = require('mustache-express');
 var request = require('request');
-
+const bodyParser = require('body-parser');
 var { Client } = require('pg');
 var connectionString = process.env.DATABASE_URL;
 
@@ -14,36 +14,66 @@ var client = new Client({
 });
 var posts;
 var allPosts;
+var myText = 'this is a new line'
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname);
 
 client.connect();
 
-client.query('SELECT * FROM posts', function(err,res){
-    if (err){
-        console.log(err.stack);
-    }
-    allPosts = res.rows;
-    console.log(allPosts);
-    client.end();
-
-    app.get("/", function(req,res){
-        console.log('Forum Loaded'); 
-        res.render('forum',{
-            "postContent":allPosts,
-            "post":function(){
-                return this.message;
-            }
+updatePost();
+function updatePost(){
+    client.query('SELECT * FROM posts', function(err,res){
+        if (err){
+            console.log(err.stack);
+        }
+        allPosts = res.rows;
+        app.get("/", function(req,res){
+            console.log('Forum Loaded'); 
+            res.render('forum',{
+                "postContent":allPosts,
+                "post":function(){
+                    return this.message;
+                }
+            });
         });
     });
-});
+}
 
 
 
-app.get('/post', function(req,res){
-    console.log('new post')
-    //
+app.post('/update', function(req,res){
+    var newPost = req.body.textarea;
+    console.log(newPost);
+    client.query("INSERT INTO posts (message) VALUES ('" + newPost+ "')"),function(err,res){
+        if (err){
+            console.log(err.stack);
+        }
+    }
+    updatePost();
+    console.log(allPosts);
+    res.render('forum',{
+        "postContent":allPosts,
+        "post":function(){
+            return this.message;
+        }
+    });
+    // client.query('SELECT * FROM posts', function(err,res){
+    //     if (err){
+    //         console.log(err.stack);
+    //     }
+    //     allPosts = res.rows;
+    //     console.log (res.rows);
+    //     client.end();
+    // });
+    // res.render('forum',{
+    //     "postContent":allPosts,
+    //     "post":function(){
+    //         return this.message;
+    //     }
+    // });
 });
 
 app.listen(port, function(){
