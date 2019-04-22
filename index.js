b114b3c1273ca02c77d3dbab6e5670bc9233ffa1
Xinyue <poyloust@gmail.com>
2019-04-22 17:39:48 -0400

    solved the lagging problem

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var mustacheExpress = require('mustache-express');
 var request = require('request');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); //to handle post request
 var { Client } = require('pg');
-var connectionString = 'postgres://vvccboedpnfrwz:d5112736a828712297b8e2d0043629df29fe74b590bd5b865198a70f7403b560@ec2-23-23-92-204.compute-1.amazonaws.com:5432/d2bt6ppaqap0hc';
-//process.env.DATABASE_URL;
+var connectionString = process.env.DATABASE_URL;
+//'postgres://vvccboedpnfrwz:d5112736a828712297b8e2d0043629df29fe74b590bd5b865198a70f7403b560@ec2-23-23-92-204.compute-1.amazonaws.com:5432/d2bt6ppaqap0hc';
+
 
 var app = express();
-var port = 5432 || process.env.PORT;
+var port = 8000 || process.env.PORT;
 var client = new Client({
-    // database: 'postgresql-silhouetted-26173', //'posts-test',
-    connectionString:connectionString,
+    database: 'postgresql-silhouetted-26173', //'posts-test'
+    connectionString: connectionString,
     ssl: true,
 });
 var posts;
@@ -20,61 +21,43 @@ var updatedPosts;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
+// next 3 lines set up mustache
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname);
 
 client.connect();
 
-updatePost();
-function updatePost(){
-    client.query('SELECT * FROM posts', function(err,res){
-        if (err){
+app.get("/", function (req, res) {
+
+    client.query('SELECT * FROM posts', function (err, res0) {
+        if (err) {
             console.log(err.stack);
         }
-        allPosts = res.rows;
-
+        allPosts = res0.rows;
         console.log(allPosts);
-        
-        app.get("/", function(req,res){
-            console.log('Forum Loaded'); 
-            res.render('forum',{
-                "postContent":allPosts,
-                "post":function(){
-                    return this.message;
-                }
-            });
+
+        res.render('forum', {
+            "postContent": allPosts,
+            "post": function () {
+                return this.message;
+            }
         });
     });
-}
+    console.log('Forum Loaded');
+});
 
 
-
-app.post('/update', async function(req,res){
+app.post('/update', function (req, res) {
     var newPost = req.body.textarea;
     console.log(newPost);
-    client.query("INSERT INTO posts (message) VALUES ('" + newPost+ "')"),function(err,res){
-        if (err){
+    client.query("INSERT INTO posts (message) VALUES ('" + newPost + "')"), function (err, res) {
+        if (err) {
             console.log(err.stack);
-        }    
+        }
     }
 
-    client.query('SELECT * FROM posts', function(err,res){
-        if (err){
-            console.log(err.stack);
-        }
-        updatedPosts = res.rows;
-
-        console.log(updatedPosts);
-
-    });
-
-    res.render('forum',{
-        "postContent":updatedPosts,
-        "post":function(){
-            return this.message;
-        }
-    });
+    res.redirect('/');
 });
 
 app.listen(port);
